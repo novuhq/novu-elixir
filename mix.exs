34cbb82f21f80435ecc6d1f61269b22cc0982511
Blake Kostner <git@btkostner.io>
2022-08-30 11:41:41 -0600

    feat: add github actions CI

--- mix.exs
+++ mix.exs
@@ -23,6 +23,10 @@ defmodule Novu.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+
+      # Development Dependencies
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false}
     ]
   end
 end
